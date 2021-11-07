6524f06a69655fd34559cc19e481d74b12f786e9
Mert Susur <mertsusur@Merts-MacBook-Pro.local>
2021-11-07 20:17:00 +0000

    updates the new contracts

--- scripts/deploy-all-funding-contractcs.js
+++ scripts/deploy-all-funding-contractcs.js
@@ -15,38 +15,34 @@ const toTimestamp = inputDays => {
 const main = async () => {
     const DeploymentManager = await hre.ethers.getContractFactory('DeploymentManager');
     const deploymentManager = await DeploymentManager.attach(getDeployedAddr(hre.network.name, 'DeploymentManager'));
-    const amountPerPayment = '500000000';
+    const amountPerPayment = '1000000000';
     const biliraTokenAddr = '0x564A341Df6C126f90cf3ECB92120FD7190ACb401';
-
     const students = [
-        { name: "Gülnisa İnankul", addr: "0xcCBC00C231422Cb016cdd90F83896a859dC0BE87" },
-        { name: "Kaan ülker", addr: "0xD951159E8E0e1e3FA526377cE1d85D1A88342440" },
-        { name: "Ömer Can Çalı", addr: "0x9C156b78b2e3C1cc6a3ffCFadce8c3418a60F452" },
-        { name: "Özgün Demiral", addr: "0xA1090a629536E3D131C71BF603A3265Ad1db5a3D" },
-        { name: "Melih Serin", addr: "0x5fb52c545068B1A31c98b6DF24385bA01E5561f2" },
-        { name: "Busenur Eren", addr: "0x1a9c5773894EadCbf796f1c34f97e789C1dE7913" },
-        { name: "Hicran Bodrumlu", addr: "0x7f5f842696Fb816ddC4524480bB0633D73dB3c69" },
-        { name: "Yusuf Alptekin Çam", addr: "0x25e307519562d0d5C76550b62b3c81BFD6057782" },
-        { name: "Tuğba Zeki", addr: "0x60C0ae3C63f2955075DB736d73a5f7Ca4645DE44" },
-        { name: "Rabia Duygu İoana Kayaner", addr: "0xd88F3770582c9b3d7AaC788041F52A5D590A26aB" },
-        { name: "Büşra Demir", addr: "0x07180D67AaaCFb7821adf34C0eafF813E826ED1f" },
-        { name: "Orkun Mahir Kılıç", addr: "0x4329AbeD9a8b987A2C0910C7fF439C4504d59f58" },
-        { name: "Ali Bozyılan", addr: "0x0BECB9534e70C14b00E6dfC44929D68808d0Bb94" },
-        { name: "Müge Keçeci", addr: "0x5238c8A74d511DEC7dE4fC12e8b2BE15bEe07b6F" },
-        { name: "Doğukan Kurşat", addr: "0x5F69E2c5f6485E9002C364CDff9B13890e590967" },
-        { name: "Bahri Atakan Yıldız", addr: "0xc7EF86D8f814846Aa58D31eCE7Cb668B958B8c4B" },
-        { name: "Merve Gündoğmuş", addr: "0x2dA1C52897c046FA26Ab4d0eB909f385eC5596C9" },
-        { name: "Dilan Can karakulak", addr: "0x19c39616f5dA156bb2A93bF5BdF0e311621052c4" },
-        { name: "hüsnü beha yıldız", addr: "0x255dC1f80284589BA9D631949632Dc6177313fcd" },
-        { name: "şeyma altundal", addr: "0x5f0E7db6EFD454d3FB4Bde01B1D1e098786F74E2" },
-        { name: "Anıl Tülü", addr: "0x1956367fA438b6c687d5Ab923DAa3d91648C6C61" },
-        { name: "Mustafa Eren Halil", addr: "0x995fA596569342FF17eE2AcFf1a5267499e379a7" },
+        { name: "Rabia Alay", addr: "0x0E8cD84972b09d26B824F11784ec07adf17f85E8" },
+        { name: "Birgül Sümer", addr: "0x72F8cC25b050E23152390CdAEe36A8848bCD56b0" },
+        { name: "Ece Harbelioğlu", addr: "0x33d12CbF3dca9e261B9a501652bdDB6015AC8e30" },
+        { name: "Rıdvan köysu", addr: "0x6b9cEcF7794657163Da56579aD2ff35B05793304" },
+        { name: "Hüseyin Oyman", addr: "0x95A38c0f8FE42Dd1fD4d0e198070bf72611F94c6" },
+        { name: "Mustafa Günkan", addr: "0xaCbA64BBe9b8A0718e38815837abC6d40eac3CaB" },
+        { name: "İpek akdemir", addr: "0x44712359c9F92F9A95C47367121C82dDdCd2A54D" },
+        { name: "Halime BABÜR", addr: "0x198F4Eb33a856C2C3e26a0455266171a1A2A1724" },
+        { name: "Tuğba Yıldırım", addr: "0x41af80969c24614F8Bd86C6ED0Fc90b333BbB6C8" },
+        { name: "Mehmet Ali Aydın", addr: "0xf1947e4bDa190D41d25069B123EE56366040fc0f" },
+        { name: "Rozelin Aslan", addr: "0x8C2b7DAFEd1b20f7aD0A7F0a679521438da8C724" },
+        { name: "Şeyda Tarım", addr: "0x58DC5f41E9176790C9b48A8A353Ec11bA45b274f" },
+        { name: "Hayrullah Can Özkan", addr: "0x08621e25a19E48E91df418428Bf6CECD6A707f09" },
+        { name: "Barış Timur", addr: "0x54811F2B9B89CeE4eB1632e88B359E0CA26F443F" },
+        { name: "Recep Aksona", addr: "0x1B70Afd01deA9446A4Fb5B19624C41A1D06Cca9b" },
+        { name: "yasemin akşeker", addr: "0x7CA1A826eB94CeD6DB04e37b6150f63F5Aa8e3e2" },
+        { name: "Ezgi Altun", addr: "0xCd1A82dC4B83D252d009313dC86baac3ea3E68f6" },
+        { name: "Zeynep Aksu", addr: "0x2e0Cb9d9CD9976Bc60548dCD70574fEF714681AE" },
+        { name: "Rozelin Aslan", addr: "0x8C2b7DAFEd1b20f7aD0A7F0a679521438da8C724" },
     ];
 
     for (student of students) {
         try {
             const tx = await deploymentManager.functions.deploy(
-                2,
+                9,
                 toTimestamp(28),
                 toEpoch(moment().add(0, 'day')),
                 amountPerPayment,

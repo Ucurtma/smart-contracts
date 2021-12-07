ed87823b3d62b450b5f3152d0b2045d9d2fd411c
Mert Susur <mail@mertsusur.com>
2021-12-07 20:31:09 +0000

    add retry when a blockchain operation fails.

--- scripts/payout-to-campaigns.js
+++ scripts/payout-to-campaigns.js
@@ -37,30 +37,36 @@ const main = async () => {
         console.log(`---------------------------`);
         let currentAddress = addrs[x];
         console.log(`Checking address: '${currentAddress}'`);
-        let fundingContract = await FundingContract.attach(currentAddress);
-        const [canWithdraw] = await fundingContract.functions.canWithdraw();
-        const [totalBalance] = await fundingContract.functions.totalBalance(biliraTokenAddr);
-        const [totalLeft] = await fundingContract.functions.totalNumberOfPayoutsLeft();
-        const [lastWithdraw] = await fundingContract.functions.lastWithdraw();
-        const [withdrawPeriod] = await fundingContract.functions.withdrawPeriod();
-        const epochTimeToWithdraw = parseInt(lastWithdraw) + parseInt(withdrawPeriod);
-        const whenToWithdraw = (epochTimeToWithdraw - Math.floor(Date.now() / 1000)) / 60 / 60;
-        const hoursToWithdraw = Math.floor(whenToWithdraw);
-        const minutesToWithdraw = Math.floor((whenToWithdraw - hoursToWithdraw) * 60);
-        console.log(`Total Balance: \t\t${parseInt(totalBalance.toString()) / 10 ** 6} TRYb`);
-        console.log(`Can withdraw: \t\t${canWithdraw.toString()}`);
-        console.log(`Total Left: \t\t${totalLeft.toString()}`);
-        console.log(`When to run: \t\t${hoursToWithdraw} hours and ${minutesToWithdraw} minutes later.`);
 
-        if (canWithdraw && parseInt(totalBalance.toString()) > 0) {
-            console.log(`Withdraw triggering for '${currentAddress}'`);
-            const tx = await fundingContract.functions.withdraw();
-            const receipt = await tx.wait();
-            console.log(`${currentAddress}:`);
-            if (receipt.transactionHash) {
-                console.log(`\t Transaction hash: \t\t${receipt.transactionHash}`);
+        try {
+            let fundingContract = await FundingContract.attach(currentAddress);
+            const [canWithdraw] = await fundingContract.functions.canWithdraw();
+            const [totalBalance] = await fundingContract.functions.totalBalance(biliraTokenAddr);
+            const [totalLeft] = await fundingContract.functions.totalNumberOfPayoutsLeft();
+            const [lastWithdraw] = await fundingContract.functions.lastWithdraw();
+            const [withdrawPeriod] = await fundingContract.functions.withdrawPeriod();
+            const epochTimeToWithdraw = parseInt(lastWithdraw) + parseInt(withdrawPeriod);
+            const whenToWithdraw = (epochTimeToWithdraw - Math.floor(Date.now() / 1000)) / 60 / 60;
+            const hoursToWithdraw = Math.floor(whenToWithdraw);
+            const minutesToWithdraw = Math.floor((whenToWithdraw - hoursToWithdraw) * 60);
+            console.log(`Total Balance: \t\t${parseInt(totalBalance.toString()) / 10 ** 6} TRYb`);
+            console.log(`Can withdraw: \t\t${canWithdraw.toString()}`);
+            console.log(`Total Left: \t\t${totalLeft.toString()}`);
+            console.log(`When to run: \t\t${hoursToWithdraw} hours and ${minutesToWithdraw} minutes later.`);
+
+            if (canWithdraw && parseInt(totalBalance.toString()) > 0) {
+                console.log(`Withdraw triggering for '${currentAddress}'`);
+                const tx = await fundingContract.functions.withdraw();
+                const receipt = await tx.wait();
+                console.log(`${currentAddress}:`);
+                if (receipt.transactionHash) {
+                    console.log(`\t Transaction hash: \t\t${receipt.transactionHash}`);
+                }
+
             }
-
+        } catch (e) {
+            console.log(`\t Address failed. Adding the address to retry list`);
+            addrs.push(currentAddress);
         }
         console.log(`--------------------------- \n`);
     }
